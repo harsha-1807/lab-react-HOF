2e36d6000e8643ed13ee7006329c59b99d00a03b
harsha-1807 <harshavardhan.sr@kalvium.community>
2024-03-06 11:26:10 +0530

    deployment completed

---
+++ hof/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import HigherOrderComponent from './components/HigherorderFunctions';
+
+function App() {
+  return (
+    <div className="App">
+      <HigherOrderComponent/>
+    </div>
+  );
+}
+
+export default App;
